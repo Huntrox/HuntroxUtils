0d35db46adf7f43bec4df323be3a92e0785cc364
Huntrox <abde.had@hotmail.com>
2022-10-25 17:28:51 +0100

    Update ExtensionMethods.cs

--- Assets/HuntroxGames/Utils/Runtime/Extensions/ExtensionMethods.cs
+++ Assets/HuntroxGames/Utils/Runtime/Extensions/ExtensionMethods.cs
@@ -113,6 +113,13 @@ namespace HuntroxGames.Utils
         {
             foreach (Transform child in transform) UnityEngine.Object.Destroy(child.gameObject);
         }
+        public static void LookAtY(this Transform transform, Vector3 point)
+        {
+            var lookPos = point - transform.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            transform.rotation = rotation;
+        }
         #endregion
     }
 }
