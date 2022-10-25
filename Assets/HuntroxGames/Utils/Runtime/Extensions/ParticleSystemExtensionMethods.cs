497953e33e134c42425766925dda3ae56f8cb7da
Huntrox <abde.had@hotmail.com>
2022-10-25 17:25:27 +0100

    added Particle System Extensions

---
+++ Assets/HuntroxGames/Utils/Runtime/Extensions/ParticleSystemExtensionMethods.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace HuntroxGames.Utils
+{
+    public static class ParticleSystemExtensionMethods 
+    {
+        public static float GetEmissionRate(this ParticleSystem particleSystem)
+        {
+            return particleSystem.emission.rate.constantMax;
+        }
+        public static void SetEmissionRate(this ParticleSystem particleSystem, float emissionRate)
+        {
+            var emission = particleSystem.emission;
+            var rate = emission.rate;
+            rate.constantMax = emissionRate;
+            emission.rate = rate;
+        }
+        public static void EnableEmission(this ParticleSystem particleSystem, bool enabled)
+        {
+            var emission = particleSystem.emission;
+            emission.enabled = enabled;
+        }
+    }
+}
