603897d90ebaef54827f505e606f13133494df2a
Huntrox <abde.had@hotmail.com>
2022-10-30 23:39:32 +0100

    Create BehaviorState.cs

---
+++ Assets/HuntroxGames/Utils/Runtime/SaveSystem/BehaviorState.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HuntroxGames.Utils
+{
+    [System.Serializable]
+    public struct BehaviorState
+    {
+        public bool wasDestroyed;
+        public List<FieldState> states;
+        public List<FieldState> customStates;
+    }
+}
