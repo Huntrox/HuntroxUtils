3091e3ee35bcf8065a8b7c3fa2ff1169d238301d
Huntrox <abde.had@hotmail.com>
2022-10-31 21:27:14 +0100

    Create SaveFile.cs

---
+++ Assets/HuntroxGames/Utils/Runtime/SaveSystem/SaveFile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace HuntroxGames.Utils
+{
+    [System.Serializable]
+    public class SaveFile
+    {
+        public string saveDisplayName;
+        public List<string> destroyedObjects;
+        public List<string> createdObjects;
+        public List<string> saveData;
+        public string saveDate;
+        public string levelName;
+    }
+}
