bb816993312961feaa7cccbf9bedf147c1a372ff
Huntrox <abde.had@hotmail.com>
2023-01-22 20:59:12 +0100

    Update TweenManager.cs

--- Assets/HuntroxGames/Utils/Runtime/Huntween/TweenManager.cs
+++ Assets/HuntroxGames/Utils/Runtime/Huntween/TweenManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +7,23 @@ namespace HuntroxGames.Utils
     public class TweenManager : Singleton<TweenManager>
     {
         private readonly List<Tween> tweens = new List<Tween>();
-
+        
 
         private void Update()
         {
-            for (int i = 0; i < tweens.Count; i++)
+            var finishedTweens = new List<Tween>();
+            for (var i = 0; i < tweens.Count; i++)
             {
-                int index = i;
+                var index = i;
                 var tween = tweens[index];
                 tween.Update(Time.deltaTime);
+                if(tween.isComplete || tween.IsKilled)
+                    finishedTweens.Add(tween);
             }
+
+            // foreach (var tween in tweens)
+            //     tweens.Remove(tween);
+            
         }
 
         public void AddTween(Tween t)
@@ -32,90 +38,4 @@ namespace HuntroxGames.Utils
         }
 
     }
-
-    public abstract class Tween
-    {
-        protected float position;
-        protected float elapsedTime;
-
-        public float duration;
-        
-        public float Position => position;
-        public Action onPlay;
-        public Action onUpdate;
-        public Action onComplete;
-
-        public bool isActive;
-        public bool isComplete;
-        public EasingFunctions.Ease ease;
-
-        public YieldInstruction WaitForTween()
-        {
-            return TweenManager.Instance.StartCoroutine(
-                TweenManager.WaitForTween(this));
-        }
-
-        public abstract void Update(float delta);
-
-    }
-
-    public class Tweener<T> : Tween where T : struct
-    {
-        public TweenGetter<T> getter;
-        public TweenSetter<T> setter;
-        public T startValue;
-        public T currentValue;
-        public T endValue;
-
-        public Tweener(TweenGetter<T> getter, TweenSetter<T> setter, T endValue, float duration,
-            EasingFunctions.Ease ease = EasingFunctions.Ease.Linear)
-        {
-            this.getter = getter;
-            this.setter = setter;
-            this.endValue = endValue;
-            this.duration = duration;
-            this.ease = ease;
-            this.startValue = this.getter();
-        }
-
-        public override void Update(float delta)
-        {
-        }
-    }
-
-    public class FloatTweener : Tweener<float>
-    {
-        public FloatTweener(TweenGetter<float> getter, TweenSetter<float> setter, float endValue, float duration, EasingFunctions.Ease ease = EasingFunctions.Ease.Linear) 
-            : base(getter, setter, endValue, duration, ease)
-        {
-        }
-
-        
-        public override void Update(float delta)
-        {
-            if(!isActive || isComplete)
-                return;
-            if (elapsedTime < duration)
-            {
-                elapsedTime += delta;
-                position = elapsedTime / duration;
-                currentValue = Mathf.Lerp(startValue, endValue,EasingFunctions.Evaluate(position, ease));
-                setter(currentValue);
-                Debug.Log(currentValue);
-                onUpdate?.Invoke();
-            }
-            else
-            {
-                isComplete = true;
-                Debug.Log("Completed");
-                onComplete?.Invoke();
-            }
-        }
-    }
-
-    public delegate T TweenGetter<out T>();
-
-    public delegate void TweenSetter<in T>(T newValue);
-
-
 }
