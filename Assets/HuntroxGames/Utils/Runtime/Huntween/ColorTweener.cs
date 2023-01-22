a1373d3d998adb06793797be0f2aede6c1f46eae
Huntrox <abde.had@hotmail.com>
2023-01-22 21:01:44 +0100

    added Quaternion and Color Tweener

---
+++ Assets/HuntroxGames/Utils/Runtime/Huntween/ColorTweener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace HuntroxGames.Utils
+{
+    public class ColorTweener : Tweener<Color>
+    {
+        public ColorTweener(TweenGetter<Color> getter, TweenSetter<Color> setter, Color endValue, float duration, EasingFunctions.Ease ease = EasingFunctions.Ease.Linear) 
+            : base(getter, setter, endValue, duration, ease)
+        {
+        }
+
+        protected override void UpdateValue()
+        {
+            currentValue = Color.Lerp(startValue, endValue, EasingFunctions.Evaluate(Position, ease));
+            setter(currentValue);
+        }
+    }
+}
