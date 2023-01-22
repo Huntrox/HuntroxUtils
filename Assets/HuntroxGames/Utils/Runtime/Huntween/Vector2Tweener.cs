a64cfd296b9c5e9a0465842b079c8093a3c29b02
Huntrox <abde.had@hotmail.com>
2023-01-22 21:01:29 +0100

    Vector 2 and 3 tweener

---
+++ Assets/HuntroxGames/Utils/Runtime/Huntween/Vector2Tweener.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace HuntroxGames.Utils
+{
+    public class Vector2Tweener : Tweener<Vector2>
+    {
+        public Vector2Tweener(TweenGetter<Vector2> getter, TweenSetter<Vector2> setter, Vector2 endValue, float duration, EasingFunctions.Ease ease = EasingFunctions.Ease.Linear)
+            : base(getter, setter, endValue, duration, ease)
+        {
+        }
+
+        protected override void UpdateValue()
+        {
+            currentValue = Vector2.Lerp(startValue, endValue, EasingFunctions.Evaluate(Position, ease));
+            setter(currentValue);
+        }
+    }
+}
---
+++ Assets/HuntroxGames/Utils/Runtime/Huntween/Vector3Tweener.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace HuntroxGames.Utils
+{
+    public class Vector3Tweener : Tweener<Vector3>
+    {
+        public Vector3Tweener(TweenGetter<Vector3> getter, TweenSetter<Vector3> setter, Vector3 endValue, float duration, EasingFunctions.Ease ease = EasingFunctions.Ease.Linear) : base(getter, setter, endValue, duration, ease)
+        {
+        }
+
+        protected override void UpdateValue()
+        {
+            currentValue = Vector3.Lerp(startValue, endValue, EasingFunctions.Evaluate(Position, ease));
+            setter(currentValue);
+        }
+    }
+}
