e1fc24037da597e0aadfd4dff02c8354841041be
Huntrox <abde.had@hotmail.com>
2023-01-23 20:33:27 +0100

    Update TweenManager.cs

--- Assets/HuntroxGames/Utils/Runtime/Huntween/TweenManager.cs
+++ Assets/HuntroxGames/Utils/Runtime/Huntween/TweenManager.cs
@@ -7,10 +7,34 @@ namespace HuntroxGames.Utils
     public class TweenManager : Singleton<TweenManager>
     {
         private readonly List<Tween> tweens = new List<Tween>();
-        
+        private readonly Dictionary<object, List<Tween>> tweenDict = new Dictionary<object, List<Tween>>();
+        private bool isActive;
+        private List<Tween> queueForAdd = new List<Tween>();
+
+        protected override void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+
+#if UNITY_EDITOR
+                if (UnityEditor.EditorApplication.isPlaying)
+#endif
+                    DontDestroyOnLoad(gameObject);
+            }
+            else
+                if(!isActive)
+                    Destroy(gameObject);
+
+            isActive = true;
+        }
 
         private void Update()
         {
+
+            for (int i = 0; i < queueForAdd.Count; i++) 
+                tweens.Add(queueForAdd[i]);
+            
             var finishedTweens = new List<Tween>();
             for (var i = 0; i < tweens.Count; i++)
             {
@@ -20,16 +44,21 @@ namespace HuntroxGames.Utils
                 if(tween.isComplete || tween.IsKilled)
                     finishedTweens.Add(tween);
             }
-
-            // foreach (var tween in tweens)
-            //     tweens.Remove(tween);
             
+            for (var index = 0; index < finishedTweens.Count; index++)
+            {
+                var tween = finishedTweens[index];
+                tweens.Remove(tween);
+            }
+
+
+            queueForAdd.Clear();
         }
 
-        public void AddTween(Tween t)
+        public void AddTween(Tween t, object source = null)
         {
             t.isActive = true;
-            tweens.Add(t);
+            queueForAdd.Add(t);
         }
         public static IEnumerator WaitForTween(Tween t)
         {
