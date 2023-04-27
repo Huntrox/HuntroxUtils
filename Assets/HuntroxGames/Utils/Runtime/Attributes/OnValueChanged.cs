9c3a147d4d7fdc42a50954f375fe1b978338945d
Huntrox <abde.had@hotmail.com>
2023-04-27 16:32:02 +0100

    Update OnValueChanged.cs

--- Assets/HuntroxGames/Utils/Runtime/Attributes/OnValueChanged.cs
+++ Assets/HuntroxGames/Utils/Runtime/Attributes/OnValueChanged.cs
@@ -1,17 +1,13 @@
 using System;
-using UnityEngine;
 
 namespace HuntroxGames.Utils
 {
     [AttributeUsage(AttributeTargets.Field,AllowMultiple = false, Inherited = true)]
     public class OnValueChangedAttribute : Attribute
     {
-        public string onValueChangedCallbackName;
+        public readonly string onValueChangedCallbackName;
         
-        public OnValueChangedAttribute(string callback)
-        {
-            onValueChangedCallbackName = callback;
-            
-        }
+        public OnValueChangedAttribute(string callback) 
+            => onValueChangedCallbackName = callback;
     }
 }
